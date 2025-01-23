a1b6077846acb5ac69d78d0311d70d5025d2873e
hyun.lee <qwe7105c@naver.com>
2025-01-23 22:21:15 +0900

    fix : 좋아요컨트롤러 분리

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,26 +1,11 @@
 import Like from '../models/like.js'; 
 import Post from '../models/post.js';
+import likeService from '../services/likeService.js';
 
 const likeController = {
   updateLikes: async (req, res) => {
-    const { post_id, user_id } = req.params;
-
     try {
-      const post = await Post.getPosts(post_id);
-
-      if (!post || post.length === 0) {
-        return res
-          .status(404)
-          .json({ success: false, message: '게시글을 찾을 수 없습니다.' });
-      }
-
-      const result = await Like.updateLikes(post_id, user_id);
-
-      if (!result.success) {
-        return res
-          .status(500)
-          .json({ success: false, message: '좋아요 업데이트 실패' });
-      }
+      const result = await likeService.updateLikes(req);
 
       return res.status(200).json({
         success: true,
@@ -28,30 +13,19 @@ const likeController = {
         result: result,
       });
     } catch (error) {
-      console.error('Error in updateLikes:', error);
-      return res
-        .status(500)
-        .json({ success: false, message: '서버 오류' });
+      next(error);
     }
   },
 
-  userLikesStatus: async (req, res) => {
-    const { post_id } = req.params;
-    const user_id = req.session.user.user_id;
-    const userId = String(user_id);
-
+  userLikesStatus: async (req, res, next) => {
     try {
-      const result = await Like.userLikesStatus(post_id, user_id);
-
+      const result = await likeService.userLikesStatus(req);
       return res.status(200).json({
         success: true,
-        result: result[0],
+        result: result,
       });
     } catch (error) {
-      console.error('Error in userLikesStatus:', error);
-      return res
-        .status(500)
-        .json({ success: false, message: '서버 오류' });
+        next(error);
     }
   },
 };
---
+++ services/likeService.js
@@ -0,0 +1,38 @@
+import Like from '../models/like.js'; 
+import Post from '../models/post.js';
+import AppError from '../utils/AppError.js';
+import Comment from '../models/comment.js';
+
+
+const updateLikes = async(req) => {
+    const { post_id, user_id } = req.params;
+
+    const post = await Post.getPosts(post_id);
+
+      if (!post || post.length === 0) {
+        throw new AppError('유효하지 않은 게시글 입니다.', 400);
+      }
+
+      const result = await Like.updateLikes(post_id, user_id);
+
+      if (!result.success) {
+        throw new AppError('좋아요 업데이트 실패', 500);
+      }
+
+
+      return result;
+}
+
+const userLikesStatus = async(req) => {
+    const { post_id } = req.params;
+    const user_id = req.session.user.user_id;
+
+    const result = await Like.userLikesStatus(post_id, user_id);
+
+    return result[0];
+}
+
+export default{
+    updateLikes,
+    userLikesStatus
+}
